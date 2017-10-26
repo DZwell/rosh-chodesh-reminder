e5a64e383f2ac8277c5f24887780441a276b5efd
Daniel Zwelling <dzwellingmusic@gmail.com>
2017-10-26 07:00:00 -0700

    Replace request with axios

--- lambdaFunction/index.js
+++ lambdaFunction/index.js
@@ -1,24 +1,22 @@
 const AWS = require('aws-sdk');
-const sunset = require('../scripts/zmanim').sunsetTime;
+const sunsetTime = require('../scripts/zmanim');
 
-exports.handler = (event, context) => {
-    const cloudwatchevents = new AWS.CloudWatchEvents();
-    const intervals = Array(3, 5, 7);
-    const nextInterval = intervals[Math.floor(Math.random()*intervals.length)];
-    const currentTime = new Date().getTime(); // UTC Time
-    const nextTime = dateAdd(currentTime, "minute", nextInterval);
+console.log(typeof sunsetTime);
 
-    const scheduleExpression = "cron(" + nextMinutes + " " + nextHours + " * * ? *)";
-    const params = {
-        Name: "YOUR CLOUDWATCH EVENT RULE NAME",
-        ScheduleExpression: scheduleExpression
-    };
-    cloudwatchevents.putRule(params, (err, data) => {
-        if (err) {
-            console.log(err, err.stack);  
-        }
-        else {
-            console.log(data);
-        }
-    })
-};
+// exports.handler = (event, context) => {
+//     const cloudwatchevents = new AWS.CloudWatchEvents();
+
+//     const scheduleExpression = `cron(${sunset.minutes})`;
+//     const params = {
+//         Name: "YOUR CLOUDWATCH EVENT RULE NAME",
+//         ScheduleExpression: scheduleExpression
+//     };
+//     cloudwatchevents.putRule(params, (err, data) => {
+//         if (err) {
+//             console.log(err, err.stack);  
+//         }
+//         else {
+//             console.log(data);
+//         }
+//     })
+// };
--- scripts/zmanim.js
+++ scripts/zmanim.js
@@ -1,16 +1,16 @@
-const isDaylightSavings = require('./daylightSavings').isDaylightSavings;
-const request = require('request');
+// const request = require('request');
+const axios = require('axios');
+const moment = require('moment-timezone');
 const twilioClient = require('../twilioClient/twilioClient');
-
 const hebCalUrl = 'http://www.hebcal.com/hebcal/?v=1&cfg=json&nx=on&year=now&maj=on&min=on';
-const sunsetUrl = 'https://api.sunrise-sunset.org/json?lat=47.608013&lng=-122.335167&formatted=1'
+const sunsetUrl = 'https://api.sunrise-sunset.org/json?lat=47.608013&lng=-122.335167&formatted=0';
 const myNumber = process.env.MY_NUMBER;
 const today = new Date();
 const tomorrow = new Date(today.getTime() + 24 * 60 * 60 * 1000).toISOString().split('T')[0]; // Grab just the date
 
 // request.get(hebCalUrl, (err, res, body) => {
 //     const jsonBody = JSON.parse(body);
-    
+
 //     jsonBody.items.forEach((item) => {
 //         if (item.category === 'roshchodesh') {
 //             const roshChodeshDate = item.date;
@@ -21,42 +21,45 @@ const tomorrow = new Date(today.getTime() + 24 * 60 * 60 * 1000).toISOString().s
 //         }
 //     });
 //     // sunsetTime();
-    
 // });
 
-Date.prototype.stdTimezoneOffset = function() {
-    const jan = new Date(this.getFullYear(), 0, 1);
-    const jul = new Date(this.getFullYear(), 6, 1);
-    return Math.max(jan.getTimezoneOffset(), jul.getTimezoneOffset());
-  }
-  
-  Date.prototype.dst = function() {
-    return this.getTimezoneOffset() < this.stdTimezoneOffset();
-  }
+function buildCronJobObject(time) {
+    const dateArray = time.split('T')[0].split('-');
+    const timeArray = time.split('T')[1].split(':');
+    const cronObject = {
+        year: dateArray[0],
+        month: dateArray[1],
+        day: dateArray[2],
+        hour: timeArray[0],
+        minute: timeArray[1]
+    };
 
-// sunset comes back as a UTC string like '3:09:47 AM'
-// Need to do some formatting
-function formatSunsetTime(time) {
-    const hour = parseInt(time.split(':')[0]);
-    if (hour)
-    
+    return cronObject;
 }
 
-const sunsetTime = () => request.get(sunsetUrl, (err, res, body) => {
-    const jsonBody = JSON.parse(body);
-    let sunset = jsonBody.results.sunset;
-    console.log(sunset);
-    formatSunsetTime(sunset);
-    if (today.dst()) {
+// const sunsetTime = () => {
+//     request.get(sunsetUrl, (err, res, body) => {
+//         if (err) {
+//             return err;
+//         }
+//         const jsonBody = JSON.parse(body);
+//         let sunset = jsonBody.results.sunset;
+//         const pstSunset = moment.tz(sunset, 'America/Los_Angeles').format();
+//         return buildCronJobObject(pstSunset);
+//     }).then();
+// };
 
-    }
-});
+const sunsetTime = () => {
+    axios.get(sunsetUrl)
+        .then(response => {
+            console.log(response.data.results.sunset);
+        }).catch(error => {
+            console.log(error);
+        });
+};
 
-sunsetTime();
+// sunsetTime();
 
-module.exports = { sunsetTime: sunsetTime };
-
-
-
+module.exports = sunsetTime();
 
 
