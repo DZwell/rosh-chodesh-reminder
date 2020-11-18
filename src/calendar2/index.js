9890415a2ff892985006f832230cb6c93d650e1c
Daniel Zwelling <dzwellingmusic@gmail.com>
2020-11-17 21:38:15 -0800

    Start over, new tokens, etc

--- src/calendar2/index.js
+++ src/calendar2/index.js
@@ -1,44 +1,38 @@
 const fs = require('fs');
 const readline = require('readline');
-const { google } = require('googleapis');
+const {google} = require('googleapis');
 
-
-'4/1AfDhmrg4-vNvv28SJyTCKuleXjbMSDxLWPKon5rxRbN9ceQPVVhrs9IpysE'
-const API_KEY = 'AIzaSyB4S6JSKMM65l1w5377qId3aY5fQ-vj0qc';
-
-// If modifying these scopes, delete credentials.json.
+// If modifying these scopes, delete token.json.
 const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
-const TOKEN_PATH = './credentials.json';
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = 'token.json';
 
 // Load client secrets from a local file.
-try {
-  const content = fs.readFileSync('./credentials.json');
+fs.readFile('credentials.json', (err, content) => {
+  if (err) return console.log('Error loading client secret file:', err);
+  // Authorize a client with credentials, then call the Google Calendar API.
   authorize(JSON.parse(content), listEvents);
-} catch (err) {
-    return console.log('Error loading client secret file:', err);
-}
+});
 
 /**
  * Create an OAuth2 client with the given credentials, and then execute the
  * given callback function.
  * @param {Object} credentials The authorization client credentials.
  * @param {function} callback The callback to call with the authorized client.
- * @return {function} if error in reading credentials.json asks for a new one.
  */
 function authorize(credentials, callback) {
-  const { client_secret, client_id, redirect_uris } = credentials.installed;
-  let token = {};
+  const {client_secret, client_id, redirect_uris} = credentials.installed;
   const oAuth2Client = new google.auth.OAuth2(
-    client_id, client_secret, redirect_uris[0]);
+      client_id, client_secret, redirect_uris[0]);
 
   // Check if we have previously stored a token.
-  try {
-    token = fs.readFileSync(TOKEN_PATH);
-  } catch (err) {
-    return getAccessToken(oAuth2Client, callback);
-  }
-  oAuth2Client.setCredentials(JSON.parse(token));
-  callback(oAuth2Client);
+  fs.readFile(TOKEN_PATH, (err, token) => {
+    if (err) return getAccessToken(oAuth2Client, callback);
+    oAuth2Client.setCredentials(JSON.parse(token));
+    callback(oAuth2Client);
+  });
 }
 
 /**
@@ -60,35 +54,33 @@ function getAccessToken(oAuth2Client, callback) {
   rl.question('Enter the code from that page here: ', (code) => {
     rl.close();
     oAuth2Client.getToken(code, (err, token) => {
-      if (err) return callback(err);
+      if (err) return console.error('Error retrieving access token', err);
       oAuth2Client.setCredentials(token);
       // Store the token to disk for later program executions
-      try {
-        fs.writeFileSync(TOKEN_PATH, JSON.stringify(token));
+      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+        if (err) return console.error(err);
         console.log('Token stored to', TOKEN_PATH);
-      } catch (err) {
-        console.error(err);
-      }
+      });
       callback(oAuth2Client);
     });
   });
 }
 
+/**
+ * Lists the next 10 events on the user's primary calendar.
+ * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+ */
 function listEvents(auth) {
-  const calendar = google.calendar({ version: 'v3', auth });
-
+  const calendar = google.calendar({version: 'v3', auth});
   calendar.events.list({
     calendarId: 'primary',
     timeMin: (new Date()).toISOString(),
     maxResults: 10,
     singleEvents: true,
     orderBy: 'startTime',
-  }, (err, data ) => {
-    if (err) {
-      return console.log('The API returned an error: ' + err);
-    }
-
-    const events = data.items;
+  }, (err, res) => {
+    if (err) return console.log('The API returned an error: ' + err);
+    const events = res.data.items;
     if (events.length) {
       console.log('Upcoming 10 events:');
       events.map((event, i) => {
