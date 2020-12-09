3497c170638dc903e114323d62eff5fc96e30179
Daniel Zwelling <daniel.zwelling@milliman.com>
2020-12-09 14:44:03 -0800

    Re-work stuff so we can call startup in .then()

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
 const fs = require('fs');
+const axios = require('axios');
 const authorize = require('./auth').authorize;
 const submitEvent = require('./events').submitEvent;
-const getRoshChodeshim = require('./zmanim').getRoshChodeshim;
+const hebCalUrl = 'http://www.hebcal.com/hebcal/?v=1&cfg=json&nx=on&year=now&maj=on&min=on';
+const monthsMap = {};
+
+axios.get(hebCalUrl)
+.then((res) => {
+  res.data.items.forEach((item) => {
+    if (item.category === 'roshchodesh') {
+      const roshChodeshDate = item.date;
+      const hebrewMonth = item.title.split(' ')[2];
+      if (monthsMap[hebrewMonth]) {
+        monthsMap[hebrewMonth].push(roshChodeshDate);
+      } else {
+        monthsMap[hebrewMonth] = [roshChodeshDate];
+      }
+    }
+  });
+
+});
 
 function startup(events) {
   fs.readFile('./creds/credentials.json', (err, content) => {
